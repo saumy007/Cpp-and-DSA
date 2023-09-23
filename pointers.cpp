6374e57dbd059acc4d53e66add477cd1f5a30c91
Saumy Sharma <saumysharma007@gmail.com>
2023-09-23 16:06:18 +0530

    learning about pointers

---
+++ pointers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num = 5;
+    cout <<endl;
+    // address of num = &
+    cout << "value of num is " << num << endl;
+    cout << "The addres of num is " << &num;
+}
