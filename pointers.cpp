56af74c04537e493aee99761f517c371bc7c5f81
Saumy Sharma <saumysharma007@gmail.com>
2023-09-23 18:38:01 +0530

    pointers theory

--- pointers.cpp
+++ pointers.cpp
@@ -6,5 +6,21 @@ int main(){
     cout <<endl;
     // address of num = &
     cout << "value of num is " << num << endl;
-    cout << "The addres of num is " << &num;
+    cout << "The addres of num is " << &num << endl;
+    // using dference operator
+    //defining the //*p = &num
+    int *p = &num;
+    cout << " the value of *p " << *p << endl;
+    cout << "the value of &num" << &num << endl;
+    cout << "the value of p" << p << endl;
+    cout << "the value of num" << num << endl;
+    //cout << "the value of num" << num;
+    (*p)++;
+    cout << "pointer Arithmatic  " << *p << endl; 
+    cout << " the valie of num is :" << num << endl;
+    int q;
+    q = *p;
+    q++;
+    cout << "Change value of q does this affect value of *p? " << q << endl;
+    cout << "Change value of q does this affect value of *p? p is here " << *p;
 }
