09c1fd2ccbc6007604f1d6a673eadeaf013a6251
Saumy Sharma <saumysharma007@gmail.com>
2023-10-01 23:36:31 +0530

    learning about dynamic memory allocation

---
+++ memoryAllocation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#define pi 3.14
+using namespace std;
+int main(){
+
+float CirArea;
+float r;
+r = 10;
+CirArea = pi*r*r;
+cout << "Area of Circle is : "<<CirArea <<endl;
+
+}
+//
