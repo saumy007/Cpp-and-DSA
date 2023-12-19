70f90506931776292126da25f3c473d672ea280b
Saumy Sharma <saumysharma007@gmail.com>
2023-12-19 21:08:52 +0530

    quicksort recursion

--- quickSortUsingRecursion.cpp
+++ quickSortUsingRecursion.cpp
@@ -1,74 +1,75 @@
 #include<iostream>
 using namespace std;
-int partition(int *arr,int s,int e){
-    int pivot =arr[s]; // definitng pivot element
 
-    //count how many elements are smaller than pivot
 
-    int count = 0;
-    for(int i = s+1;i<=s;i++){
-        if(arr[i]<=pivot){
-            count++;
+int partition( int arr[], int s, int e) {
+
+    int pivot = arr[s];
+
+    int cnt = 0;
+    for(int i = s+1; i<=e; i++) {
+        if(arr[i] <=pivot) {
+            cnt++;
         }
     }
-    //placing pivor at right element
 
-    int pivotIndex = s + count;
-    swap(arr[pivotIndex],arr[s]);
+    //place pivot at right position
+    int pivotIndex = s + cnt;
+    swap(arr[pivotIndex], arr[s]);
 
-    //left and right part
+    //left and right wala part smbhal lete h 
+    int i = s, j = e;
 
-    int i = s,j = e;
+    while(i < pivotIndex && j > pivotIndex) {
 
-    while(i<pivotIndex && j> pivotIndex){
-        while(arr[i]<pivot){
+        while(arr[i] <= pivot) 
+        {
             i++;
         }
-        while(arr[j]>pivot){
+
+        while(arr[j] > pivot) {
             j--;
         }
 
-        if(i<pivotIndex && j> pivotIndex){
-            swap(arr[i++],arr[j--]);
+        if(i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
         }
-        
+
     }
+
     return pivotIndex;
 
 }
 
-void quickSort(int* arr,int s,int e){
+void quickSort(int arr[], int s, int e) {
 
-//base case
-if(s>=e)
-return ;
+    //base case
+    if(s >= e) 
+        return ;
 
-//do partitiion
+    //partitioon karenfe
+    int p = partition(arr, s, e);
 
-int p = partition(arr,s,e);
+    //left part sort karo
+    quickSort(arr, s, p-1);
 
-//left part sort
+    //right wala part sort karo
+    quickSort(arr, p+1, e);
 
-quickSort(arr,s,p+1);
-
-//right part sort
-
-quickSort(arr,p+1,e);
 }
-int main(){
-    int arr[]={4,8,6,2,7,12,69,54};
-    int size = 8;
-    cout << "Array Before Sorting"<<endl;
-    for(int i =0;i<size;i++){
-        cout<<arr[i]<<" ";
-    }cout<<endl;
-    
-    quickSort(arr,0,size-1);
 
-    cout << "Array After Sorting"<<endl;
-    for(int i =0;i<size;i++){
-        cout<<arr[i]<<" ";
-    }
+int main() {
 
-    
+    int arr[10] = {2,4,1,6,9 ,9,9,9,9,9};
+    int n = 10;
+
+    quickSort(arr, 0, n-1);
+
+    for(int i=0; i<n; i++) 
+    {
+        cout << arr[i] << " ";
+    } cout << endl;
+
+
+    return 0;
 }
