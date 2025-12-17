3754fa8c767c7116745a874baafb447eab521cff
Saumy.Sharma <saumy.sharma@ril.com>
2025-12-17 17:20:21 +0530

    learning Kadanes Algorithm

---
+++ Kadane_Algorrithm.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+int maxSummation(vector<int> &arr) {
+    int maxSum = arr[0];
+
+    int currentSum = arr[0];
+
+    for (int i = 1; i < arr.size(); i++) {
+        currentSum = max(arr[i], currentSum + arr[i]);
+        maxSum = max(maxSum, currentSum);
+    }
+    return maxSum;
+}
+
+
+int main(){
+
+    vector <int> arr = {-2};
+    //create a variable to store the maximum sum
+    int maxSum = maxSummation(arr);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+
+    return 0;
+
+}
